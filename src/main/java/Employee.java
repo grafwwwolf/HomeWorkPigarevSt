e0dc445c78e967019903640578773eedc5ef30a4
Stanislav <grafwwwolf@yandex.ru>
2021-09-27 20:21:07 +0300

    Final commit update

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -17,21 +17,10 @@ public class Employee {
         this.salary = salary;
         this.age = age;
     }
-    public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
-        employees[0] = new Employee("Pigarev Stanislav Mikhailovich", "Java Developer", "mail@mail.ru", "89991111111", 100000, 33);
-        employees[1] = new Employee("Kurochkin Aleksey Nikolaevich", "Manager", "kural@mail.ru", "89992222222", 60000, 38);
-        employees[2] = new Employee("Ivanov Sergey Petrovich", "Driver", "ivsep@mail.ru", "89993333333", 45000, 52);
-        employees[3] = new Employee("Gorbach Dmitry Viktorovich", "Director", "gordyi64@mail.ru", "89994444444", 450000, 50);
-        employees[4] = new Employee("Mineeva Elena Borisovna", "Chief accountant", "mineevaglbuh@mail.ru", "89995555555", 85000, 41);
 
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i].age > 40) {
-                employees[i].printEmp();
-            }
-        }
+    public int getAge() {
+        return age;
     }
-
 
     public void printEmp() {
         System.out.println("Employee name: " + fullName + ", post: " + post + ", email: " + email + ", phone: " + phone + ", salary: " + salary + ", age: " + age + ".");
